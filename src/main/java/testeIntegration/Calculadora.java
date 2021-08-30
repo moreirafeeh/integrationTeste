f3e0285ea07a709f5f415d324ffd995a1c302f19
unknown <kimimarux@hotmail.com>
2021-08-30 19:09:29 -0300

    testeBug

--- src/main/java/testeIntegration/Calculadora.java
+++ src/main/java/testeIntegration/Calculadora.java
@@ -2,4 +2,25 @@ package testeIntegration;
 
 public class Calculadora {
 
+	public static void somar1() {
+		
+		int i = 1;
+		int k = 2;
+		int soma;
+		
+		soma = i + k;
+		
+	}
+	
+	public static void somar2() {
+			
+			int i = 1;
+			int k = 2;
+			int soma;
+			
+			soma = i + k;
+			
+		}
+	
+	
 }
