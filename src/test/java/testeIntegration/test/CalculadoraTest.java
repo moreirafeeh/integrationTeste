2ae419b1eb02b78ac32be452d2250ee8d103d6e2
unknown <kimimarux@hotmail.com>
2021-08-31 10:22:14 -0300

    calculadorasTestadas

--- src/test/java/testeIntegration/test/CalculadoraTest.java
+++ src/test/java/testeIntegration/test/CalculadoraTest.java
@@ -1,13 +1,24 @@
-package testeIntegration;
+package testeIntegration.test;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import testeIntegration.Calculadora;
 
 public class CalculadoraTest {
 	
 	@Test
 	public void TestarCalculo() {
 		int soma = Calculadora.somar1();
+		
+		Assert.assertEquals(3, soma);
+		
+	}
+	
+
+	@Test
+	public void TestarCalculoSoma() {
+		int soma = Calculadora.somar2();
 		
 		Assert.assertEquals(3, soma);
 		
